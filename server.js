7c74bbf206bd577ca5df697636c2e117fe41a20d
Cody B. Daig <cody@daig.me>
2015-04-25 13:56:56 -0600

    Basic Express Website, mainly just taking baby steps

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 'use strict';
 
 // This will be the start of the app. 
+
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+	res.send('Welcome to MeanBoss!');
+});
+
+app.listen(3000);
