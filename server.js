383881f50fcd1c4191ec2f83233873e706ae0f68
Cody B. Daig <cody@daig.me>
2015-04-25 22:45:48 -0600

    Initial Config Files

--- server.js
+++ server.js
@@ -2,15 +2,6 @@
 
 // This will be the start of the app. 
 
-var express = require('express');
-var test = require('meanbossio');
+var meanboss = require('meanbossio');
 
-var app = express();
-
-app.get('/', function(req, res) {
-	res.send('Welcome to MeanBoss!');
-});
-
-app.listen(3000);
-
-test.print();
+meanboss.start();
